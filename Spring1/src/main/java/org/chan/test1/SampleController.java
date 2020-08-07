f01d8a4de175f27f4af06060a16c6f41b9cbc2db
YoungChanK <kyc3161@naver.com>
2020-08-07 16:34:15 +0900

    3.Get/Post 방식 어노테이션으로 처리 기능

--- Spring1/src/main/java/org/chan/test1/SampleController.java
+++ Spring1/src/main/java/org/chan/test1/SampleController.java
@@ -4,16 +4,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //자바파일을 controller로 인식시켜주기 위해 @Controller를 사용
 @Controller
+//그전까진 view에 있는 파일을 메핑했다하면 view안에 다른 파일이 있을시 위에서 value값을 미리 선언해준다   (여기선 GET방식만 가능)
+// view/doA   ==>>  view/board/doA
+@RequestMapping(value="board")
+
 public class SampleController {
 	private static final Logger logger=LoggerFactory.getLogger(SampleController.class);
 	
-	@RequestMapping(value="doA")
-	public void doA() {
-		logger.info("@RequestMapping doA가 호출되었습니다 ");
+	@RequestMapping(value="doW", method = RequestMethod.POST)
+	public void doW(@RequestParam("id") String id, @RequestParam("password")String password, @RequestParam("name")String name,@RequestParam("phone") int phone) {
+		logger.info("@RequestMapping doW가 호출되었습니다 ");
 	}
+	@RequestMapping(value="doM" , method = RequestMethod.POST)
+	public void doM() {
+		logger.info("@RequestMapping doM가 호출되었습니다 ");
+	}
+	@RequestMapping(value="doD" , method = RequestMethod.POST)
+	public void doD() {
+		logger.info("@RequestMapping doD가 호출되었습니다 ");
+	}
+	@RequestMapping(value="doR", method = RequestMethod.GET)
+	public void doR() {
+		logger.info("@RequestMapping doR가 호출되었습니다 ");
+	}
+
 /*	@RequestMapping("doB")
 	public void doB() {
 		logger.info("doB called...............");
---
+++ Spring1/src/main/webapp/WEB-INF/views/board/doM.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+doM.jsp 화면으동 이동되었습니다.
+</body>
+</html>
