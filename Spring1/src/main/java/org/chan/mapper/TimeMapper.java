edf73e96066faaa1a225dff39f05f50e74252bed
YoungChanK <kyc3161@naver.com>
2020-08-07 15:15:55 +0900

    1.HttpServletRequest, HttpServletResponse를 거의 사용할 필요없이 기능구현

--- Spring1/src/main/java/org/chan/mapper/TimeMapper.java
+++ Spring1/src/main/java/org/chan/mapper/TimeMapper.java
@@ -1,7 +1,7 @@
 package org.chan.mapper;
 
 import org.apache.ibatis.annotations.Select;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 public interface TimeMapper {
 	//현재시간과 날짜를 조회하는 sql
--- Spring1/src/main/java/org/chan/test1/SampleController.java
+++ Spring1/src/main/java/org/chan/test1/SampleController.java
@@ -1,6 +1,6 @@
 package org.chan.test1;
 
-/*import org.slf4j.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SampleController {
 	private static final Logger logger=LoggerFactory.getLogger(SampleController.class);
 	
-	@RequestMapping("doA")
+	@RequestMapping(value="doA")
 	public void doA() {
 		logger.info("@RequestMapping doA가 호출되었습니다 ");
-	}*/
+	}
 /*	@RequestMapping("doB")
 	public void doB() {
 		logger.info("doB called...............");
