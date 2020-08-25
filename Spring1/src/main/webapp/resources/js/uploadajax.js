ddd9a92a18bf1cd149058ee108db96c16d4deeb0
YoungChanK <kyc3161@naver.com>
2020-08-25 15:14:10 +0900

    파일업로드 제한

--- Spring1/src/main/webapp/resources/js/uploadajax.js
+++ Spring1/src/main/webapp/resources/js/uploadajax.js
@@ -2,6 +2,26 @@
  * 
  */
 $(document).ready(function(){
+	// .뒤에 exe,sh,zip,alz 를 제한하겠다 ->파일형식 제한
+	//정규식
+	var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$")
+	var maxSize=5242880;			//파일 사이즈(5MB)
+	
+	function checkExtension(fileName, fileSize){
+		if(fileSize>=maxSize){
+			alert("파일 사이즈 초과");
+			return false;
+		}
+		if(regex.test(fileName)){
+			alert("파일 업로드할수 없습니다");
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
+	
 	$(".fileDrop").on("dragenter dragover",function(e){
 		e.preventDefault();
 //		alert("drag");
@@ -11,10 +31,13 @@ $(document).ready(function(){
 		e.preventDefault();
 //		alert("drag");
 		var files = e.originalEvent.dataTransfer.files;
+//		console.log(files[0].name);
+//		console.log(files[0].size);
 		var formData = new FormData(); //uploadAjax.jsp에 form태그가 없다. 대체할 무언가가 필요한데 , FormData()
-		
 		for(var i=0; i<files.length;i++){
-			
+			if(!checkExtension(files[i].name,files[i].size)){
+				return false;
+			}
 		formData.append("file",files[i]);
 		}
 //		var file = files[0];
